1fc1c4fd5644647b885676f9d79bd7299293dad0
hagiya0121 <koazu0121@gmail.com>
2024-02-14 16:37:36 +0900

    条件式を追加

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,3 +1,12 @@
 20.times do |number|
-  puts number + 1
+  number += 1
+  if number % 3 == 0 && number % 5 == 0
+    puts "FizzBuzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+  elsif number % 5 == 0
+    puts "Buzz"
+  else
+    puts number
+  end
 end
