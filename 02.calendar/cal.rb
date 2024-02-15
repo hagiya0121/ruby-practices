8e7df1c99c73d5c9f6152ff2a18d8e89efbbfe5a
hagiya0121 <koazu0121@gmail.com>
2024-02-15 19:06:01 +0900

    改行する

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -3,4 +3,7 @@ require 'date'
 puts Date.today.strftime("%-m月 %Y").center(20)
 puts "日 月 火 水 木 金 土"
 last_day = Date.new(2024, 2, -1).day
-last_day.times { |day| print day + 1 }
+last_day.times do |day|
+  print day + 1
+  puts "\n" if day.even?
+end
