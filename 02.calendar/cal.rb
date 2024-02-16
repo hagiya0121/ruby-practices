c652c8d9e3706704381c0427fd84af629435f991
hagiya0121 <koazu0121@gmail.com>
2024-02-16 16:32:43 +0900

    日にちの間隔を調整

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -5,7 +5,7 @@ puts "日 月 火 水 木 金 土"
 week_day = Date.new(2024, 2, 1).wday
 last_day = Date.new(2024, 2, -1).day
 (1..last_day).each do |day|
-  print day
+  print "#{day}".rjust(2) + "\s"
   puts "\n" if week_day == 6
   week_day == 6 ? week_day = 0 : week_day += 1
 end
