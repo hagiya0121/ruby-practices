cee1cbb45fb7cfa78ed8a431bf2c190bbb81508d
hagiya0121 <koazu0121@gmail.com>
2024-02-16 16:17:24 +0900

    土曜日に改行する

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -2,8 +2,10 @@ require 'date'
 
 puts Date.today.strftime("%-m月 %Y").center(20)
 puts "日 月 火 水 木 金 土"
+week_day = Date.new(2024, 2, 1).wday
 last_day = Date.new(2024, 2, -1).day
 last_day.times do |day|
   print day + 1
-  puts "\n" if day.even?
+  puts "\n" if week_day == 6
+  week_day == 6 ? week_day = 0 : week_day += 1
 end
