52e33551c336c90c9e1df628a5ca873704f46241
hagiya0121 <koazu0121@gmail.com>
2024-02-16 17:07:30 +0900

    初日の曜日に合わせてオフセット

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,9 +1,12 @@
 require 'date'
 
-puts Date.today.strftime("%-m月 %Y").center(20)
-puts "日 月 火 水 木 金 土"
 week_day = Date.new(2024, 2, 1).wday
 last_day = Date.new(2024, 2, -1).day
+first_weekday_offset = week_day * 3
+
+puts Date.today.strftime("%-m月 %Y").center(20)
+puts "日 月 火 水 木 金 土"
+print "\s" * first_weekday_offset
 (1..last_day).each do |day|
   print "#{day}".rjust(2) + "\s"
   puts "\n" if week_day == 6
