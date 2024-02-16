a3fa0e491900798db70bb51e3e81a3a62240ad76
hagiya0121 <koazu0121@gmail.com>
2024-02-16 16:21:26 +0900

    ループを処理を変更

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -4,8 +4,8 @@ puts Date.today.strftime("%-m月 %Y").center(20)
 puts "日 月 火 水 木 金 土"
 week_day = Date.new(2024, 2, 1).wday
 last_day = Date.new(2024, 2, -1).day
-last_day.times do |day|
-  print day + 1
+(1..last_day).each do |day|
+  print day
   puts "\n" if week_day == 6
   week_day == 6 ? week_day = 0 : week_day += 1
 end
