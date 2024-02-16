7183f40925bdbe320ebece1af8554ae336ed2bbc
hagiya0121 <koazu0121@gmail.com>
2024-02-16 17:26:44 +0900

    今日の日にちの文字色と背景色を反転

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -2,14 +2,16 @@ require 'date'
 
 week_day = Date.new(2024, 2, 1).wday
 last_day = Date.new(2024, 2, -1).day
+today = Date.today.day
 first_weekday_offset = week_day * 3
 
 puts Date.today.strftime("%-m月 %Y").center(20)
 puts "日 月 火 水 木 金 土"
 print "\s" * first_weekday_offset
 (1..last_day).each do |day|
+  day = "\e[7m#{day}\e[0m" if day == today
   print "#{day}".rjust(2) + "\s"
   puts "\n" if week_day == 6
   week_day == 6 ? week_day = 0 : week_day += 1
 end
-puts "\n\n" 
+puts "\n\n"
