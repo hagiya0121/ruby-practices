d24b3300005a8465b410a9996b4bd8224c966e57
hagiya0121 <koazu0121@gmail.com>
2024-02-17 10:28:21 +0900

    コマンドラインからの引数をハッシュに代入するように変更

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -2,18 +2,17 @@ require 'date'
 require 'optparse'
 
 today = Date.today
-month = today.month
-year = today.year
+options = { month: today.month, year: today.year}
 opt = OptionParser.new
-opt.on("-m", "--month MONTH", Integer, "月を指定") { |m| month = m }
-opt.on("-y", "--year YEAR", Integer, "年を指定") { |y| year = y }
+opt.on("-m", "--month MONTH", Integer, "月を指定") { |month| options[:month] = month }
+opt.on("-y", "--year YEAR", Integer, "年を指定") { |year| options[:year] = year }
 opt.parse!(ARGV)
 
-puts "#{month}月 #{year}".center(20)
+puts "#{options[:month]}月 #{options[:year]}".center(20)
 puts "日 月 火 水 木 金 土"
 
-first_date = Date.new(year, month, 1)
-last_date = Date.new(year, month, -1)
+first_date = Date.new(options[:year], options[:month], 1)
+last_date = Date.new(options[:year], options[:month], -1)
 print " " * first_date.wday * 3
 (first_date..last_date).each do |date|
   if date == today
