d506ad2f9731edce12468dae8b92fb7847849fcd
hagiya0121 <koazu0121@gmail.com>
2024-02-17 10:00:09 +0900

    最初の曜日のオフセット計算を変更

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -9,18 +9,17 @@ opt.on("-m", "--month MONTH", Integer, "月を指定") { |m| month = m }
 opt.on("-y", "--year YEAR", Integer, "年を指定") { |y| year = y }
 opt.parse!(ARGV)
 
-
 puts "#{month}月 #{year}".center(20)
 puts "日 月 火 水 木 金 土"
+
 first_date = Date.new(year, month, 1)
 last_date = Date.new(year, month, -1)
-first_weekday_offset = first_date.wday * 3
-print "\s" * first_weekday_offset
+print " " * first_date.wday * 3
 (first_date..last_date).each do |date|
   if date == today
     print "\e[7m#{date.day}\e[0m"
   else
-    print "#{date.day}".rjust(2) + "\s"
+    print "#{date.day}".rjust(2) + " "
   end
   puts "\n" if date.saturday?
 end
