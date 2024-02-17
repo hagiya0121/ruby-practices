ecf7179e439a01e3b7635bc62126765a3df05f1a
hagiya0121 <koazu0121@gmail.com>
2024-02-17 09:27:16 +0900

    ループ処理を変更

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -10,17 +10,16 @@ opt.on("-y", "--year YEAR", Integer, "年を指定") { |y| year = y }
 opt.parse!(ARGV)
 
 first_weekday = Date.new(year, month, 1).wday
-last_day = Date.new(year, month, -1).day
 first_weekday_offset = first_weekday * 3
 
 puts "#{month}月 #{year}".center(20)
 puts "日 月 火 水 木 金 土"
 print "\s" * first_weekday_offset
-(1..last_day).each do |day|
-  is_today = today.year == year && today.month == month && today.day == day
-  day = "\e[7m#{day}\e[0m" if is_today
-  print "#{day}".rjust(2) + "\s"
-  puts "\n" if first_weekday == 6
-  first_weekday == 6 ? first_weekday = 0 : first_weekday += 1
+first_date = Date.new(year, month, 1)
+last_date = Date.new(year, month, -1)
+(first_date..last_date).each do |date|
+  day = "\e[7m#{date}\e[0m" if date == today
+  print "#{date.day}".rjust(2) + "\s"
+  puts "\n" if date.saturday?
 end
 puts "\n\n"
