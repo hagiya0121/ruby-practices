5b80d10b5c302e1bb0c63a03596069d0e4c98116
hagiya0121 <koazu0121@gmail.com>
2024-02-20 05:16:37 +0900

    10フレーム目は普通に加算

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -22,10 +22,10 @@ point = 0
 numeric_scores = scores.map { |s| s == 'X' ? 10 : s.to_i }
 frames.each.with_index(1) do |frame, index|
   throw_count = index * 2 - (strike_count + 1)
-  if frame[0] == 10 # strike
+  if frame[0] == 10 && index <= 9  # strike
     strike_count += 1
     point +=  10 + numeric_scores[throw_count] + numeric_scores[throw_count + 1]
-  elsif frame.sum == 10 # spare
+  elsif frame.sum == 10 && index <= 9 # spare
     point += 10 + numeric_scores[throw_count + 1]
   else
     point += frame.sum
