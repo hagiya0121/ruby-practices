ddd27c6ee4f304538d3b52719da29cdf127faf23
hagiya0121 <koazu0121@gmail.com>
2024-02-22 03:09:59 +0900

    スコアを判定

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -5,9 +5,18 @@ scores = score.split(',').map { |s| s == 'X' ? 10 : s.to_i }
 
 frame = 0
 shot_count = 0
+point = 0
 scores.each.with_index do |score, index|
+  point += score
   shot_count += 1
-  if shot_count == 2 || score == 10
+  if score == 10
+    point += scores[index + 1] + scores[index + 2]
+    frame += 1
+    shot_count = 0
+  end
+
+  if shot_count == 2
+    point += scores[index + 1] if scores[index] + scores[index - 1] == 10
     frame += 1
     shot_count = 0
   end
