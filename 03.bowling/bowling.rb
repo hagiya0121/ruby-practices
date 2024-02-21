38c46c846e753a69161de447488bc54e09c36d5a
hagiya0121 <koazu0121@gmail.com>
2024-02-22 02:27:37 +0900

    フレームを計算

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,34 +1,14 @@
 #!/usr/bin/env ruby
 
 score = ARGV[0]
-scores = score.split(',')
-shots = []
-scores.each do |s|
-  if s == 'X'
-    shots << 10
-    shots << 0
-  else
-    shots << s.to_i
+scores = score.split(',').map { |s| s == 'X' ? 10 : s.to_i }
+
+frame = 0
+shot_count = 0
+scores.each.with_index do |score, index|
+  shot_count += 1
+  if shot_count == 2 || score == 10
+    frame += 1
+    shot_count = 0
   end
 end
-
-frames = []
-shots.each_slice(2) do |s|
-  frames << s
-end
-
-point = 0
-frames.each.with_index(1) do |frame, frame_number|
-  if frame[0] == 10 && frame_number <= 9  # strike
-    if frames[frame_number][0] == 10
-      point += 10 + frames[frame_number][0] + frames[frame_number + 1][0]
-    else
-      point += 10 + frames[frame_number].sum
-    end
-  elsif frame.sum == 10 && frame_number <= 9 # spare
-    point += 10 + frames[frame_number][0]
-  else
-    point += frame.sum
-  end
-end
-puts point
