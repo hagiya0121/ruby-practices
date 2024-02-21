349eaa0570cadc24f785a14eb576837eac6fa78e
hagiya0121 <koazu0121@gmail.com>
2024-02-22 01:45:05 +0900

    framesからスコアを計算

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -17,17 +17,16 @@ shots.each_slice(2) do |s|
   frames << s
 end
 
-strike_count = 0
 point = 0
-shot_scores = scores.map { |s| s == 'X' ? 10 : s.to_i }
 frames.each.with_index(1) do |frame, frame_number|
-  # 現在のフレームの1投目が全体の何投目かを計算
-  shot_count = frame_number * 2 - (strike_count + 1)
   if frame[0] == 10 && frame_number <= 9  # strike
-    strike_count += 1
-    point +=  10 + shot_scores[shot_count] + shot_scores[shot_count + 1]
+    if frames[frame_number][0] == 10
+      point += 10 + frames[frame_number][0] + frames[frame_number + 1][0]
+    else
+      point += 10 + frames[frame_number].sum
+    end
   elsif frame.sum == 10 && frame_number <= 9 # spare
-    point += 10 + shot_scores[shot_count + 1]
+    point += 10 + frames[frame_number][0]
   else
     point += frame.sum
   end
