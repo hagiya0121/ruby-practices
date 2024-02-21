d40b74e277c02a4075229036b09980607f17f1d4
hagiya0121 <koazu0121@gmail.com>
2024-02-22 03:41:07 +0900

    10フレーム目はスコア判定を行わない

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -8,16 +8,17 @@ shot_count = 0
 point = 0
 scores.each.with_index do |score, index|
   point += score
-  shot_count += 1
-  if score == 10
-    point += scores[index + 1] + scores[index + 2]
-    frame += 1
-    shot_count = 0
-  end
+  next if frame == 9
 
+  shot_count += 1
   if shot_count == 2
     point += scores[index + 1] if scores[index] + scores[index - 1] == 10
     frame += 1
     shot_count = 0
+  elsif score == 10
+    point += scores[index + 1] + scores[index + 2]
+    frame += 1
+    shot_count = 0
   end
 end
+puts point
