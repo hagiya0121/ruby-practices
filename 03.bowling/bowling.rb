66987cfc1aecbcef8fc8be91de28914b40f121f3
hagiya0121 <koazu0121@gmail.com>
2024-02-22 03:55:40 +0900

    変数名の変更

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -3,26 +3,26 @@
 score = ARGV[0]
 scores = score.split(',').map { |s| s == 'X' ? 10 : s.to_i }
 
-frame = 0
+current_frame = 0
 shot_count = 0
-point = 0
+total_points = 0
 scores.each.with_index do |score, index|
-  point += score
-  next if frame == 9
+  total_points += score
+  next if current_frame == 9
 
   shot_count += 1
 
   if shot_count == 1 && score == 10
-    point += scores[index + 1] + scores[index + 2]
+    total_points += scores[index + 1] + scores[index + 2]
   end
 
   if shot_count == 2 && scores[index] + scores[index - 1] == 10
-    point += scores[index + 1]
+    total_points += scores[index + 1]
   end
 
   if shot_count == 2 || score == 10
-    frame += 1
+    current_frame += 1
     shot_count = 0
   end
 end
-puts point
+puts total_points
