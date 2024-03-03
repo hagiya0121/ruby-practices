60f210085be7c411d777be8a4221341914ee5b58
hagiya0121 <koazu0121@gmail.com>
2024-03-03 15:11:13 +0900

    ファイルを分割

--- 04.ls/list_files.rb
+++ 04.ls/list_files.rb
@@ -1,4 +1,10 @@
 #!/usr/bin/env ruby
 
-files = Dir.entries('../')
-puts files
+files = Dir.entries('./test_dir1').sort
+column = 3
+col_files = files.size % column == 0 ? files.size / column : files.size / column + 1
+
+slice_files = []
+files.each_slice(col_files) do |file|
+  slice_files << file
+end
