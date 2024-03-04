49298abb22adae537bf00f0fabd0d11c34e13c8a
hagiya0121 <koazu0121@gmail.com>
2024-03-04 12:45:15 +0900

    ファイル数が0でもエラーが出ないように修正

--- 04.ls/list_files.rb
+++ 04.ls/list_files.rb
@@ -4,13 +4,11 @@ files = Dir.entries('./test_dir1').sort
 column = 3
 col_files = files.size % column == 0 ? files.size / column : files.size / column + 1
 
-slice_files = []
-files.each_slice(col_files) do |file|
-  slice_files << file
-end
+
+slice_files = files.each_slice(col_files).to_a if files.size != 0
 
 col_files.times do |col|
-  column.times do |row|
+  slice_files.size.times do |row|
     print slice_files[row][col], " "
   end
   puts
