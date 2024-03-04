e8ac139fdfea8b6d3c8445dff8660a6e6e3c8931
hagiya0121 <koazu0121@gmail.com>
2024-03-04 13:36:30 +0900

    隠しファイルを削除

--- 04.ls/list_files.rb
+++ 04.ls/list_files.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-files = Dir.entries('./test_dir1').sort
+files = Dir.entries('./test_dir1').sort.delete_if {|file| file.start_with?('.')}
 column = 3
 col_files = files.size % column == 0 ? files.size / column : files.size / column + 1
 
@@ -11,7 +11,7 @@ max_length = slice_files.flatten.max_by {|name| name.length}.length
 
 col_files.times do |col|
   slice_files.size.times do |row|
-    print slice_files[row][col].ljust(max_length + 4)
+    print slice_files[row][col]&.ljust(max_length + 4)
   end
   puts
 end
