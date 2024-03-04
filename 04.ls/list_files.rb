1fa9c4665bdc2e208c2caf43925aeffe0f78e2a5
hagiya0121 <koazu0121@gmail.com>
2024-03-04 13:26:48 +0900

    出力を整形

--- 04.ls/list_files.rb
+++ 04.ls/list_files.rb
@@ -7,9 +7,11 @@ col_files = files.size % column == 0 ? files.size / column : files.size / column
 
 slice_files = files.each_slice(col_files).to_a if files.size != 0
 
+max_length = slice_files.flatten.max_by {|name| name.length}.length
+
 col_files.times do |col|
   slice_files.size.times do |row|
-    print slice_files[row][col], " "
+    print slice_files[row][col].ljust(max_length + 4)
   end
   puts
 end
