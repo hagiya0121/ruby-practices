0d0bf4f014d09ec77e88882b982e57a1693b48ac
hagiya0121 <koazu0121@gmail.com>
2024-03-05 13:59:00 +0900

    メゾッドを定義

--- 04.ls/list_files.rb
+++ 04.ls/list_files.rb
@@ -1,22 +1,25 @@
 #!/usr/bin/env ruby
 
 file_path = ARGV[0] || './'
-if File.file?(file_path)
-  puts file_path
-  exit
-end
+(puts file_path; exit) if File.file?(file_path)
+
 files = Dir.entries(file_path).sort.delete_if {|file| file.start_with?('.')}
-column = 3
-col_files = files.size % column == 0 ? files.size / column : files.size / column + 1
+exit if files.size == 0
 
-
-slice_files = files.each_slice(col_files).to_a if files.size != 0
-
-max_length = slice_files.flatten.max_by {|name| name.length}.length
-
-col_files.times do |col|
-  slice_files.size.times do |row|
-    print slice_files[row][col]&.ljust(max_length + 4)
-  end
-  puts
+def max_length(files)
+  files.max_by {|name| name.length}.length
 end
+
+def print_files(files, column)
+  output_rows = files.size % column == 0 ? files.size / column : files_count / column + 1
+  slice_files = files.each_slice(output_rows).to_a
+
+  output_rows.times do |col|
+    slice_files.size.times do |row|
+      print slice_files[row][col]&.ljust(max_length(files) + 4)
+    end
+    puts
+  end
+end
+
+print_files(files, 3)
