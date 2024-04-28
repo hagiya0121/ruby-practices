e020d67ab4138b1960140f268fa84ebb80f0d4e7
hagiya0121 <koazu0121@gmail.com>
2024-04-28 13:03:21 +0900

    feat: ファイルの行数、単語数、バイト数を取得する

--- 05.wc/word_count.rb
+++ 05.wc/word_count.rb
@@ -1,2 +1,13 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
+
+file_path = ARGV[0]
+
+File.open(file_path, 'r') do |file|
+  words = 0; lines = 0; bytes = file.size
+  file.each_line do |line|
+    words += line.split(" ").size
+    lines += 1
+  end
+  puts lines, words, bytes
+end
