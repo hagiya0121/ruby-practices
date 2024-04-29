70f16e53adbeda91432ff2366f379f45bb8aadb9
hagiya0121 <koazu0121@gmail.com>
2024-04-29 11:53:20 +0900

    feat: オプションを追加

--- 05.wc/word_count.rb
+++ 05.wc/word_count.rb
@@ -1,17 +1,30 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'optparse'
+
 PADDING = 8
+
+options = { line: false, word: false, char: false }
+opt = OptionParser.new
+opt.on('-l') { |v| options[:line] = v }
+opt.on('-w') { |v| options[:word] = v }
+opt.on('-c') { |v| options[:char] = v }
+opt.parse!(ARGV)
+
+options.each_key { |key| options[key] = true } if options.values.none?
 
 file_path = ARGV[0]
 
 File.open(file_path, 'r') do |file|
-  lines = 0
-  words = 0
-  bytes = file.size
+  file_counts = { lines: 0, words: 0, bytes: file.size }
   file.each_line do |line|
-    words += line.split(' ').size
-    lines += 1
+    file_counts[:words] += line.split(' ').size
+    file_counts[:lines] += 1
   end
-  puts [lines, words, bytes].map { |i| i.to_s.rjust(PADDING) }.join + " #{file_path}"
+  results = []
+  results << file_counts[:lines] if options[:line]
+  results << file_counts[:words] if options[:word]
+  results << file_counts[:bytes] if options[:char]
+  puts results.map { |i| i.to_s.rjust(PADDING) }.join + " #{file_path}"
 end
