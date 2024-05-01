e3da2c680393d8215e4bf2ecd57d1987652d8d86
hagiya0121 <koazu0121@gmail.com>
2024-05-01 09:56:40 +0900

    feat: 複数ファイルの引数に対応

--- 05.wc/word_count.rb
+++ 05.wc/word_count.rb
@@ -14,17 +14,31 @@ opt.parse!(ARGV)
 
 options.each_key { |key| options[key] = true } if options.values.none?
 
-file_path = ARGV[0]
-
-File.open(file_path, 'r') do |file|
-  file_counts = { lines: 0, words: 0, bytes: file.size }
-  file.each_line do |line|
-    file_counts[:words] += line.split(' ').size
-    file_counts[:lines] += 1
-  end
-  results = []
-  results << file_counts[:lines] if options[:line]
-  results << file_counts[:words] if options[:word]
-  results << file_counts[:bytes] if options[:char]
-  puts results.map { |i| i.to_s.rjust(PADDING) }.join + " #{file_path}"
+def main(options)
+  print_file_count(options)
 end
+
+def print_file_count(options)
+  results = ARGV.map {|path| calc_file_count(path, options)}
+  results.each_with_index do |e, i|
+    puts e.map { |a| a.to_s.rjust(PADDING) }.join + "\s#{ARGV[i]}"
+  end
+  puts results.transpose.map {|a| a.sum.to_s.rjust(PADDING)}.join + "\stotal"
+end
+
+def calc_file_count(file_path, options)
+  results = []
+  File.open(file_path, 'r') do |file|
+    file_counts = { lines: 0, words: 0, bytes: file.size }
+    file.each_line do |line|
+      file_counts[:words] += line.split(' ').size
+      file_counts[:lines] += 1
+    end
+    results << file_counts[:lines] if options[:line]
+    results << file_counts[:words] if options[:word]
+    results << file_counts[:bytes] if options[:char]
+  end
+  results
+end
+
+main(options)
