27cfed2242974160ceae7a9873d08d2c22b8b022
hagiya0121 <koazu0121@gmail.com>
2024-07-15 12:29:33 +0900

    refactor: frame自身でstrike判定をするように変更

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -11,13 +11,17 @@ class Frame
   def score(frames)
     return frame_score if last?
 
-    if shots[0].strike?
+    if strike?
       frame_score + next_two_shots_score(frames)
     elsif spare?
       frame_score + next_shot_score(frames)
     else
       frame_score
     end
+  end
+
+  def strike?
+    shots[0].strike?
   end
 
   def last?
@@ -27,7 +31,7 @@ class Frame
   private
 
   def spare?
-    !shots[0].strike? && @shots[0..1].sum(&:score) == 10
+    !strike? && @shots[0..1].sum(&:score) == 10
   end
 
   def frame_score
@@ -40,7 +44,7 @@ class Frame
 
   def next_two_shots_score(frames)
     next_frame = frames[@frame_number + 1]
-    if next_frame.shots[0].strike? && !next_frame.last?
+    if next_frame.strike? && !next_frame.last?
       10 + frames[@frame_number + 2].shots[0].score
     else
       next_frame.shots[0..1].sum(&:score)
